6b621697f46e17c57cedb3fd2eb2e03529f43506
Heloisa Sperendio <heloisasperendio@gmail.com>
2024-04-07 17:59:40 -0300

    first commit

--- Program.java
+++ Program.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java. util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,45 +10,24 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        try {
 
-        Scanner sc = new Scanner (System.in);
-        List<TaxPayer> list = new ArrayList<>();
-
-        System.out.print("Enter the number of tax payers: ");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <=n; i++){
-            System.out.println("Payer #" + i + " data: ");
-            System.out.print("Individual or Company (i/c)? ");
-            char ch = sc.next().charAt(0);
-            if (ch == 'i'){
-                System.out.print("Name: ");
-                sc.nextLine();
-                String name = sc.nextLine();
-                System.out.print("Anual income: ");
-                double anualIncome = sc.nextDouble();
-                System.out.print("Health expenditures: ");
-                double healthExpenditures = sc.nextDouble();
-                TaxPayer tax = new Individual(name, anualIncome, healthExpenditures);
-                list.add(tax);
-            }
-            else {
-                System.out.print("Name: ");
-                sc.nextLine();
-                String name = sc.nextLine();
-                System.out.print("Anual income: ");
-                double anualIncome = sc.nextDouble();
-                System.out.print("Number of employees: ");
-                int numberOfEmployees = sc.nextInt();
-                TaxPayer tax = new Company(name, anualIncome, numberOfEmployees);
-                list.add(tax);
-            }
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.print(vect[position]);
         }
-        System.out.println();
-        System.out.println("TAXES PAID ");
-        for (TaxPayer tax : list ) {
-            System.out.println(tax);
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid position!");
         }
+
+        catch (InputMismatchException e){
+            System.out.println("Input Error!");
+        }
+
+        System.out.println("End of Program");
+
+        sc.close();
 
     }
 }
