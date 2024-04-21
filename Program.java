1bcaa7e7eaadc89a8c67551b5fdef05c3d05afee
Heloisa Sperendio <heloisasperendio@gmail.com>
2024-04-21 11:05:48 -0300

    Calculadora

--- Program.java
+++ Program.java
@@ -9,19 +9,51 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        PrintService <Integer> ps = new PrintService<>();
+        System.out.println("Qual operacao deseja realizar? ");
+        System.out.println("[1] - SOMA");
+        System.out.println("[2] - SUBTRACÃO");
+        System.out.println("[3] - MULTIPLICACÃO");
+        System.out.println("[4] - DIVISÃO");
+        System.out.println("[5] - SAIR");
 
-        System.out.print("How many values? ");
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            Integer value = sc.nextInt();
-            ps.addValue(value);
-        }
+        Operacoes op = new Operacoes();
 
-        ps.print();
-        Integer x = ps.first();
-        System.out.println("First: " + x);
+        while (n !=5){
+            if(n == 1){
+
+                System.out.println("Digite os números da operacão: ");
+                double n1 = sc.nextDouble();
+                double n2 = sc.nextDouble();
+                op.setNumeros(n1,n2);
+                System.out.println("Soma = " + op.soma());
+            }
+            else if(n == 2){
+
+                System.out.println("Digite os números da operacão");
+                double n1 = sc.nextDouble();
+                double n2 = sc.nextDouble();
+                op.setNumeros(n1,n2);
+                System.out.println("Subtracão = " + op.subtracao());
+            }
+            else if(n == 3){
+
+                System.out.println("Digite os números da operacão");
+                double n1 = sc.nextDouble();
+                double n2 = sc.nextDouble();
+                op.setNumeros(n1,n2);
+                System.out.println("Multiplicacão = " + op.multiplicacao());
+            }
+            else if (n == 4){
+
+                System.out.println("Digite os números da operacão");
+                double n1 = sc.nextDouble();
+                double n2 = sc.nextDouble();
+                op.setNumeros(n1,n2);
+                System.out.println("Divisão = " + op.divisao());
+            }
+        }
 
         sc.close();
     }
