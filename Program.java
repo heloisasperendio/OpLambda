0f79c76384f7c75e8859660610c0b0073d6a62e8
Heloisa Sperendio <heloisasperendio@gmail.com>
2024-04-26 17:49:06 -0300

    Exercicio Set

--- Program.java
+++ Program.java
@@ -1,7 +1,8 @@
-//238
 package org.example;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Program {
 
@@ -9,53 +10,32 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Qual operacao deseja realizar? ");
-        System.out.println("[1] - SOMA");
-        System.out.println("[2] - SUBTRACÃO");
-        System.out.println("[3] - MULTIPLICACÃO");
-        System.out.println("[4] - DIVISÃO");
-        System.out.println("[5] - SAIR");
+        Set<Integer> set = new HashSet<>();
 
+        System.out.print("Quantos estudantes para o curso A? ");
         int n = sc.nextInt();
-
-        Operacoes op = new Operacoes();
-
-        while (n !=5){
-            if(n == 1){
-
-                System.out.println("Digite os números da operacão: ");
-                double n1 = sc.nextDouble();
-                double n2 = sc.nextDouble();
-                op.setNumeros(n1,n2);
-                System.out.println("Soma = " + op.soma());
-            }
-            else if(n == 2){
-
-                System.out.println("Digite os números da operacão");
-                double n1 = sc.nextDouble();
-                double n2 = sc.nextDouble();
-                op.setNumeros(n1,n2);
-                System.out.println("Subtracão = " + op.subtracao());
-            }
-            else if(n == 3){
-
-                System.out.println("Digite os números da operacão");
-                double n1 = sc.nextDouble();
-                double n2 = sc.nextDouble();
-                op.setNumeros(n1,n2);
-                System.out.println("Multiplicacão = " + op.multiplicacao());
-            }
-            else if (n == 4){
-
-                System.out.println("Digite os números da operacão");
-                double n1 = sc.nextDouble();
-                double n2 = sc.nextDouble();
-                op.setNumeros(n1,n2);
-                System.out.println("Divisão = " + op.divisao());
-            }
+        for (int i =0; i< n; i++){
+            int codigo = sc.nextInt();
+            set.add(codigo);
+        }
+        System.out.print("Quantos estudantes para o curso B?");
+        int n1 = sc.nextInt();
+        for (int i =0; i< n1; i++){
+            int codigo = sc.nextInt();
+            set.add(codigo);
+        }
+        System.out.print("Quantos estudantes para o curso C?");
+        int n2 = sc.nextInt();
+        for (int i =0; i< n2; i++){
+            int codigo = sc.nextInt();
+            set.add(codigo);
         }
 
-        sc.close();
-    }
 
+
+        System.out.println("Total students: " + set.size());
+
+
+
+    }
 }
